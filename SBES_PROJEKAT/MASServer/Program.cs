253dd1caf822971a466da4cbc76e1ad97211111e
Emily <emiliabalazs.ki@gmail.com>
2023-01-09 20:31:05 +0100

    MASServer, digitalni potpis

---
+++ SBES_PROJEKAT/MASServer/Program.cs
@@ -0,0 +1,46 @@
+﻿using ServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9999/SecurityService";
+
+            //Autentifikacija tipa Windows
+            binding.Security.Mode = SecurityMode.Transport;
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+            binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+
+            ServiceHost serviceHost = new ServiceHost(typeof(SecurityService));
+            serviceHost.AddServiceEndpoint(typeof(ISecurityService), binding, address);
+
+            try
+            {
+                serviceHost.Open();
+                Console.WriteLine("WCFService is started.\nPress <enter> to stop ...");
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[ERROR] {0}", e.Message);
+                Console.WriteLine("[StackTrace] {0}", e.StackTrace);
+            }
+            finally
+            {
+                serviceHost.Close();
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SBES_PROJEKAT/MASServer/SecurityService.cs
@@ -0,0 +1,40 @@
+﻿using Manager;
+using ServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using HashAlgorithm = Manager.HashAlgorithm;
+
+namespace MASServer
+{
+    public class SecurityService : ISecurityService
+    {
+        public void TestMetoda(string message, byte[] signature)
+        {
+
+
+            string clienName = Formater.ParseName(ServiceSecurityContext.Current.PrimaryIdentity.Name.ToLower());
+
+            string clientNameSign = clienName + "_sign";
+            X509Certificate2 certificate = CertManager.GetCertificateFromStorage(StoreName.TrustedPeople,
+                StoreLocation.LocalMachine, clientNameSign);
+
+            /// Verify signature using SHA1 hash algorithm
+            if (DigitalSignature.Verify(message, HashAlgorithm.SHA1, signature, certificate))
+            {
+                Console.WriteLine("Sign is valid");
+                Console.WriteLine(message);
+                //WriteToTXT.WriteToTxt(message);
+            }
+            else
+            {
+                Console.WriteLine("Sign is invalid");
+            }
+        }  
+    }
+}
