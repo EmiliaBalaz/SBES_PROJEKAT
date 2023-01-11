de0a862b5dd3abdd510e7891d9c6a0a578e092b5
Tijanana <104691452+Tijanana@users.noreply.github.com>
2023-01-11 19:27:26 +0100

    Serverska strana XMS servera

--- SBES_PROJEKAT/XMSServer/Program.cs
+++ SBES_PROJEKAT/XMSServer/Program.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Manager;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.ServiceModel;
+using System.ServiceModel.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +15,47 @@ namespace XMSServer
     {
         static void Main(string[] args)
         {
+            string srvCertCN = Formater.ParseName(WindowsIdentity.GetCurrent().Name.ToLower());
+            Console.WriteLine(srvCertCN);
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+
+            string address = "net.tcp://localhost:9997/Services";
+            ServiceHost host = new ServiceHost(typeof(Services));
+            host.AddServiceEndpoint(typeof(IServices), binding, address);
+
+            ///Custom validation mode enables creation of a custom validator - CustomCertificateValidator
+            host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;
+            host.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServiceCertValidator();
+
+            ///If CA doesn't have a CRL associated, WCF blocks every client because it cannot be validated
+            host.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+
+            ///Set appropriate service's certificate on the host. Use CertManager class to obtain the certificate based on the "srvCertCN"
+            host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN);
+
+            try
+            {
+                host.Open();
+                Console.WriteLine("WCFService is started.\nPress <enter> to stop ...");
+
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[ERROR] {0}", e.Message);
+                Console.WriteLine("[StackTrace] {0}", e.StackTrace);
+            }
+            finally
+            {
+                host.Close();
+            }
+
+
+            Console.ReadLine();
+          
+
         }
     }
 }
